adde4f2c8d5cf116528d287c0a026adad3013aec
Joshy Garcia <joshy.garcia@lc.cuny.edu>
2023-04-17 20:42:54 -0400

    Landing redirects

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,15 +1,38 @@
-import { useState} from "react"
+import { useState, useEffect } from "react"
 import '../css/landing.css'
+import { useNavigate } from 'react-router-dom';
 
 const Landing = () => {
+  let navigate = useNavigate()
+
+  const handleLogin = () => {
+    navigate("/login")
+  }
+
+  const handleRegister = () => {
+    navigate("/register")
+  }
+
+  useEffect(() => {
+    navigate('/');
+  }, []);
+
   return (
     <div className="landing-container">
         <div className="landing-login">
-            <button className="landing-login-btn">Login</button>
+            <button className="landing-login-btn"
+            onClick={handleLogin}
+            >
+              Login
+            </button>
         </div>
         <div className="landing-main-content">
-            <img src="../../public/level-up-icon-white.png" alt="" />
-            <button className="landing-register">Register</button>
+            {/* <img src="../../public/level-up-icon-white.png" alt="" /> */}
+            <button className="landing-register"
+            onClick={handleRegister}
+            >
+            Register
+            </button>
         </div>
         <div className="landing-text">
           <h1 className="landing-title">
