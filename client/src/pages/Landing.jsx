50dbe86f8ea04301d4d2bf49ce571c073e3ac110
Joshy Garcia <joshy.garcia@lc.cuny.edu>
2023-05-06 06:15:05 -0400

    new landing

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
-import { useNavigate } from 'react-router-dom';
-import { Card, TextInput, Button, Flex } from "@tremor/react";
+import { useNavigate } from "react-router-dom"
+import { Card, TextInput, Button, Flex } from "@tremor/react"
 
 const Landing = () => {
   let navigate = useNavigate()
@@ -14,41 +14,118 @@ const Landing = () => {
   }
 
   useEffect(() => {
-    navigate('/');
-  }, []);
+    navigate("/")
+  }, [])
 
   return (
-    <Flex 
-      className="min-h-screen"
-      justifyContent="center"
-      alignItems="center"
-    >
-        <div className="w-32 flex flex-col">
-            <Button className="mb-5"
-              onClick={handleLogin}
-              size="xl"
-              variant="secondary"
-            >
-              Login
-            </Button>
-            {/* <img src="../../public/level-up-icon-white.png" alt="" /> */}
-            <Button className=""
-            onClick={handleRegister}
-            size="xl"
-            >
-            Register
-            </Button>
+    <div className="xs:text-center flex min-h-screen flex-col bg-gray-900 text-white md:text-left">
+      <header className="flex items-center justify-between bg-gray-800 p-4 shadow-md">
+        <div className="flex items-center">
+          <img
+            src={"/level-up-icon-white.png"}
+            alt="LevelUp Company Logo"
+            className="ml-4 h-20"
+          />
         </div>
-        <div className="">
-          <h1 className="">
-
-          </h1>
-          <p className="">
-            
+        <div className="flex space-x-4">
+          <Button
+            className="hover:bg-blue-500 hover:text-white"
+            variant="secondary"
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+          <Button
+            className="bg-purple-500 hover:bg-purple-700"
+            onClick={handleRegister}
+          >
+            Register
+          </Button>
+        </div>
+      </header>
+      <main className="mt-12 flex flex-grow flex-col items-center justify-center space-y-16 bg-gray-900">
+        <section className="flex w-11/12 flex-col items-center space-y-4 p-8 md:w-3/4 md:flex-row md:space-y-0 lg:w-1/2 xl:w-7/12 2xl:w-7/12 ">
+          <div className="flex flex-col md:w-1/2">
+            <h2 className="mb-4 text-3xl font-semibold">
+              Plan and Execute Unforgettable Events
+            </h2>
+            <p className="mb-4 text-gray-300">
+              Want to plan and execute a memorable event without the stress?
+              Look no further than{" "}
+              <span className="capitalize text-purple-700 ">LevelUP</span>. Our
+              event management software and expert managers make it easy to
+              plan, organize, and execute any event. Start planning your dream
+              event today with{" "}
+              <span className="capitalize text-purple-700 ">LevelUP</span>.
+            </p>
+            <Button className="self-center bg-purple-500 hover:bg-purple-700">
+              Register
+            </Button>
+          </div>
+          <div className="justify-end md:flex md:w-1/2">
+            <img
+              src="https://via.placeholder.com/400"
+              alt="Services"
+              className="w-full rounded-md md:w-3/4"
+            />
+          </div>
+        </section>
+        <section className="flex w-11/12 flex-col items-center space-y-4 p-8 md:w-3/4 md:flex-row-reverse md:space-y-0 lg:w-1/2 xl:w-7/12 2xl:w-7/12 ">
+          <div className="md:w-1/2">
+            <h2 className="mb-4 text-3xl font-semibold">
+              Effortlessly Plan Your Next Event
+            </h2>
+            <p className="mb-4 text-gray-300">
+              Planning an event can be overwhelming, but with{" "}
+              <span className="capitalize text-purple-700 ">LevelUP</span>, it
+              doesn't have to be. Our event management software simplifies every
+              aspect of event planning, from venue selection to logistics
+              coordination. Sign up today to experience hassle-free event
+              planning with{" "}
+              <span className="capitalize text-purple-700 ">LevelUP</span>.
+            </p>
+          </div>
+          <div className="md:flex md:w-1/2 md:justify-start">
+            <img
+              src="https://via.placeholder.com/400"
+              alt="Innovative Solutions"
+              className="w-full rounded-md md:w-3/4"
+            />
+          </div>
+        </section>
+        <section className="flex w-11/12 flex-col items-center space-y-4 p-8 md:w-3/4 md:flex-row md:space-y-0 lg:w-1/2 xl:w-7/12 2xl:w-7/12 ">
+          <div className="md:w-1/2">
+            <h2 className="mb-4 text-3xl font-semibold">
+              Make Your Event Stand Out
+            </h2>
+            <p className="mb-4 text-gray-300">
+              Want to host an event that truly stands out?{" "}
+              <span className="capitalize text-purple-700 ">LevelUP</span> can
+              help. Our event management software and expert team give you
+              access to the best venues, catering, and production services, so
+              you can create a unique and memorable event. Get started today and
+              make your event dreams a reality with{" "}
+              <span className="capitalize text-purple-700 ">LevelUP</span>.
+            </p>
+          </div>
+          <div className="justify-end md:flex md:w-1/2">
+            <img
+              src="https://via.placeholder.com/400"
+              alt="Customer Support"
+              className="w-full rounded-md md:w-3/4 2xl:m-0"
+            />
+          </div>
+        </section>
+      </main>
+      <footer className="mt-8 bg-gray-800 py-4">
+        <div className="container mx-auto px-4 text-center">
+          <p className="text-sm text-gray-300">
+            © {new Date().getFullYear()} Your Company Name. All rights reserved.
           </p>
         </div>
-    </Flex>
+      </footer>
+    </div>
   )
 }
 
-export default Landing;
+export default Landing
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -30,102 +30,116 @@ function Register() {
   }
 
   return (
-    <Flex
-      justifyContent="center"
-      alignItems="center"
-      className="min-h-screen flex-col"
-    >
-      <Text className="mb-6 text-center text-2xl text-black">Register</Text>
-      <Card decoration="bottom" decorationColor="blue" className="max-w-lg">
-        <form
-          className="flex flex-col justify-center align-middle"
-          onSubmit={handleSubmit}
-        >
-          <TextInput
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            placeholder="Company Name"
-            className="mb-4"
+    <Flex className="min-h-screen" justifyContent="center" alignItems="center">
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        className="min-h-screen flex-col"
+      >
+        <Text className="mb-6 text-center text-2xl text-black">Register</Text>
+        <div decoration="bottom" decorationColor="blue" className="max-w-lg">
+          <form
+            className="flex flex-col justify-center align-middle"
+            onSubmit={handleSubmit}
+          >
+            <TextInput
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              placeholder="Company Name"
+              className="mb-4"
+            />
+            <TextInput
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+              placeholder="Username"
+              className="mb-4"
+            />
+            <TextInput
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className="mb-4"
+              placeholder="Email"
+            />
+            <TextInput
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleInputChange}
+              className="mb-4"
+            />
+            <TextInput
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              value={formData.confirmPassword}
+              onChange={handleInputChange}
+              className="mb-4"
+            />
+            <TextInput
+              id="address"
+              name="address"
+              value={formData.address}
+              onChange={handleInputChange}
+              placeholder="Address"
+              className="mb-4"
+            />
+            <TextInput
+              id="city"
+              name="city"
+              value={formData.city}
+              onChange={handleInputChange}
+              placeholder="City"
+              className="mb-4"
+            />
+            <TextInput
+              id="state"
+              name="state"
+              value={formData.state}
+              onChange={handleInputChange}
+              placeholder="State"
+              className="mb-4"
+            />
+            <TextInput
+              id="zip"
+              name="zip"
+              value={formData.zip}
+              onChange={handleInputChange}
+              placeholder="Zip"
+              className="mb-4"
+            />
+            <Button type="submit" className="mt-4 w-32 self-center">
+              Register
+            </Button>
+            <Text className="text-md mt-6 text-center text-gray-400">
+              Already have an account?{" "}
+              <Link to="/login" className="text-blue-500">
+                Login
+              </Link>
+            </Text>
+          </form>
+        </div>
+      </Flex>
+      <div
+        className="flex h-screen w-full flex-col items-center justify-center bg-slate-900 text-center"
+        id="register-link"
+      >
+        <a href="/" className="pointer cursor-auto">
+          <img
+            src="../public/level-up-icon-white.png"
+            alt="Level Up Logo"
+            className="h-32 w-32"
           />
-          <TextInput
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-            placeholder="Username"
-            className="mb-4"
-          />
-          <TextInput
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            className="mb-4"
-            placeholder="Email"
-          />
-          <TextInput
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleInputChange}
-            className="mb-4"
-          />
-          <TextInput
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            placeholder="Confirm Password"
-            value={formData.confirmPassword}
-            onChange={handleInputChange}
-            className="mb-4"
-          />
-          <TextInput
-            id="address"
-            name="address"
-            value={formData.address}
-            onChange={handleInputChange}
-            placeholder="Address"
-            className="mb-4"
-          />
-          <TextInput
-            id="city"
-            name="city"
-            value={formData.city}
-            onChange={handleInputChange}
-            placeholder="City"
-            className="mb-4"
-          />
-          <TextInput
-            id="state"
-            name="state"
-            value={formData.state}
-            onChange={handleInputChange}
-            placeholder="State"
-            className="mb-4"
-          />
-          <TextInput
-            id="zip"
-            name="zip"
-            value={formData.zip}
-            onChange={handleInputChange}
-            placeholder="Zip"
-            className="mb-4"
-          />
-          <Button type="submit" className="mt-4 w-32 self-center">
-            Register
-          </Button>
-          <Text className="text-md mt-6 text-center text-gray-400">
-            Already have an account?{" "}
-            <Link to="/login" className="text-blue-500">
-              Login
-            </Link>
-          </Text>
-        </form>
-      </Card>
+        </a>
+      </div>
     </Flex>
   )
 }
