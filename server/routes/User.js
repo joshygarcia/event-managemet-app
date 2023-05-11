4503418d8011daa96de3351582c4772dc5bb1074
Joshy Garcia <joshy.garcia@lc.cuny.edu>
2023-05-11 05:50:22 -0400

    replace bcrypt with bcryptjs

--- server/routes/User.js
+++ server/routes/User.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const router = express.Router()
 const { User, Manager, Company } = require("../models")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const { validateTokenAdmin } = require("../middlewares/AuthMiddleware")
 
