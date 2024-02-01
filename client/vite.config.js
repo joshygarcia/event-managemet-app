5498ba9cd6179121e4d4a52b9f9dfc9fbe255139
Joshy Garcia <joshy.garcia@lc.cuny.edu>
2024-02-01 16:42:06 -0500

    change url to production

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,30 +2,15 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  const isProduction = mode === "production"
-  console.log("isProduction", isProduction)
-
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      proxy: isProduction
-        ? {
-            // production server proxy
-            "/api": {
-              target: "https://levelup-server.onrender.com",
-              changeOrigin: true,
-              secure: false,
-            },
-          }
-        : {
-            // local server proxy
-            "/api": {
-              target: "http://localhost:3000",
-              changeOrigin: true,
-              secure: false,
-            },
-          },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://levelup-server.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
     },
-  })
-}
+  },
+})
